44236442d615c24c5178c265b9cc4cbf1ad24b00
Bertrand Moulard and Rasheed Abdul-Aziz <pair+translated+squeedee@pivotallabs.com>
2014-04-09 17:04:23 -0400

    Use offline copies of go binaries
    [#68836698]

--- lib/cloud_foundry/buildpack_packager.rb
+++ lib/cloud_foundry/buildpack_packager.rb
@@ -8,10 +8,20 @@ module CloudFoundry
         /\.gitignore/
     ]
 
+    DEPENDENCIES = [
+      "http://go.googlecode.com/files/go1.1.1.linux-amd64.tar.gz",
+      "http://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz",
+      "http://go.googlecode.com/files/go1.1.linux-amd64.tar.gz",
+      "http://go.googlecode.com/files/go1.2.1.linux-amd64.tar.gz",
+      "http://go.googlecode.com/files/go1.2.linux-amd64.tar.gz",
+
+    ]
+
     class << self
       def package
         Dir.mktmpdir do |temp_dir|
           copy_buildpack_contents(temp_dir)
+          download_dependencies(temp_dir) unless ENV['ONLINE']
           compress_buildpack(temp_dir)
         end
       end
@@ -20,6 +30,16 @@ module CloudFoundry
 
       def copy_buildpack_contents(target_path)
         run_cmd "cp -r * #{target_path}"
+      end
+
+      def download_dependencies(target_path)
+        dependency_path = File.join(target_path, 'dependencies')
+
+        run_cmd "mkdir -p #{dependency_path}"
+
+        DEPENDENCIES.each do |uri|
+          run_cmd "cd #{dependency_path}; curl #{uri} -O"
+        end
       end
 
       def in_pack?(file)
