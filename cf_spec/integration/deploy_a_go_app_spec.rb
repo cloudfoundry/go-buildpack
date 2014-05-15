b6dede331deb960ae19477f03461a74efdf9fe52
Aaron Triantafyllidis and Chris Haueter <pair+atriantafyllidis+chaueter@pivotallabs.com>
2014-05-15 11:05:50 -0400

    Restructred go app fixtures to be consistent

--- cf_spec/integration/deploy_a_go_app_spec.rb
+++ cf_spec/integration/deploy_a_go_app_spec.rb
@@ -15,4 +15,12 @@ describe 'deploying a go app', :go_buildpack do
       expect(app.homepage_html).to include "hello, heroku"
     end
   end
+
+  it 'deploys the app with dependencies' do
+    Machete.deploy_app("go_app_with_dependencies", :go) do |app|
+      expect(app).to be_staged
+      expect(app.logs).to include "Hello from foo!"
+      expect(app.homepage_html).to include "hello, world"
+    end
+  end
 end
