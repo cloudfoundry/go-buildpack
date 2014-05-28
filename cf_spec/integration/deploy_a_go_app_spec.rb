2648bf23e234da7d01327dd2b673336ce5e885f4
Aaron Triantafyllidis and Jordi Noguera <pair+atriantafyllidis+jordi@pivotallabs.com>
2014-05-28 17:24:35 -0400

    don't need to pass language when pushing app in tests

--- cf_spec/integration/deploy_a_go_app_spec.rb
+++ cf_spec/integration/deploy_a_go_app_spec.rb
@@ -3,21 +3,21 @@ require "spec_helper"
 
 describe 'deploying a go app', :go_buildpack do
   it "makes the homepage available" do
-    Machete.deploy_app("go_app", :go) do |app|
+    Machete.deploy_app("go_app/src/go_app") do |app|
       expect(app).to be_staged
       expect(app.homepage_html).to include "go, world"
     end
   end
 
   it 'deploys the heroku hello world' do
-    Machete.deploy_app("go_heroku_example", :go) do |app|
+    Machete.deploy_app("go_heroku_example/src/go_heroku_example") do |app|
       expect(app).to be_staged
       expect(app.homepage_html).to include "hello, heroku"
     end
   end
 
   it 'deploys the app with dependencies' do
-    Machete.deploy_app("go_app_with_dependencies", :go) do |app|
+    Machete.deploy_app("go_app_with_dependencies/src/go_app_with_dependencies") do |app|
       expect(app).to be_staged
       expect(app.logs).to include "Hello from foo!"
       expect(app.homepage_html).to include "hello, world"
