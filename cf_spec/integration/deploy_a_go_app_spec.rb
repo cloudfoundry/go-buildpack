d856ce6fd27b14210abab6cc3302de636a536253
Aaron Triantafyllidis and Tim Johnson <pair+atriantafyllidis+buddhistpirate@pivotallabs.com>
2014-06-17 17:56:03 -0400

    Cache mode does not try to reach the internet
    [#73420832]

--- cf_spec/integration/deploy_a_go_app_spec.rb
+++ cf_spec/integration/deploy_a_go_app_spec.rb
@@ -1,26 +1,73 @@
 $: << 'cf_spec'
-require "spec_helper"
+require 'spec_helper'
 
-describe 'deploying a go app', :go_buildpack do
-  it "makes the homepage available" do
-    Machete.deploy_app("go_app/src/go_app") do |app|
-      expect(app).to be_staged
-      expect(app.homepage_html).to include "go, world"
+describe 'CF Go Buildpack' do
+  subject(:app) { Machete.deploy_app(app_name) }
+
+  context 'with cached buildpack dependencies' do
+    context 'in an offline environment', if: Machete::BuildpackMode.offline? do
+      context 'app has dependencies' do
+        let(:app_name) { 'go_app_with_dependencies/src/go_app_with_dependencies' }
+
+        specify do
+          expect(app).to be_staged
+          expect(app.logs).to include 'Hello from foo!'
+          expect(app.homepage_html).to include 'hello, world'
+          expect(app).to have_no_internet_traffic
+        end
+      end
+
+      context 'app has no dependencies' do
+        let(:app_name) { 'go_app/src/go_app' }
+
+        specify do
+          expect(app).to be_staged
+          expect(app.homepage_html).to include 'go, world'
+          expect(app).to have_no_internet_traffic
+        end
+      end
+
+      context 'heroku example' do
+        let(:app_name) { 'go_heroku_example/src/go_heroku_example' }
+
+        specify do
+          expect(app).to be_staged
+          expect(app.homepage_html).to include 'hello, heroku'
+          expect(app).to have_no_internet_traffic
+        end
+      end
     end
   end
 
-  it 'deploys the heroku hello world' do
-    Machete.deploy_app("go_heroku_example/src/go_heroku_example") do |app|
-      expect(app).to be_staged
-      expect(app.homepage_html).to include "hello, heroku"
-    end
-  end
+  context 'without cached buildpack dependencies' do
+    context 'in an online environment', if: Machete::BuildpackMode.online? do
+      context 'app has dependencies' do
+        let(:app_name) { 'go_app_with_dependencies/src/go_app_with_dependencies' }
 
-  it 'deploys the app with dependencies' do
-    Machete.deploy_app("go_app_with_dependencies/src/go_app_with_dependencies") do |app|
-      expect(app).to be_staged
-      expect(app.logs).to include "Hello from foo!"
-      expect(app.homepage_html).to include "hello, world"
+        specify do
+          expect(app).to be_staged
+          expect(app.logs).to include 'Hello from foo!'
+          expect(app.homepage_html).to include 'hello, world'
+        end
+      end
+
+      context 'app has no dependencies' do
+        let(:app_name) { 'go_app/src/go_app' }
+
+        specify do
+          expect(app).to be_staged
+          expect(app.homepage_html).to include 'go, world'
+        end
+      end
+
+      context 'heroku example' do
+        let(:app_name) { 'go_heroku_example/src/go_heroku_example' }
+
+        specify do
+          expect(app).to be_staged
+          expect(app.homepage_html).to include 'hello, heroku'
+        end
+      end
     end
   end
 end
