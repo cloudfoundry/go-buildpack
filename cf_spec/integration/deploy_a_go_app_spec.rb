81b990a326595a1e5a4891f06b1ecc766c082e35
Matt Horan <mhoran@pivotal.io>
2015-07-24 11:24:10 -0400

    Use tags instead of conditionals for online/offline tests
    [#99786478]
    
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- cf_spec/integration/deploy_a_go_app_spec.rb
+++ cf_spec/integration/deploy_a_go_app_spec.rb
@@ -6,7 +6,7 @@ describe 'CF Go Buildpack' do
   let(:browser) { Machete::Browser.new(app) }
 
   context 'with cached buildpack dependencies' do
-    context 'in an offline environment', if: Machete::BuildpackMode.cached? do
+    context 'in an offline environment', :cached do
       context 'app has dependencies' do
         let(:app_name) { 'go_app_with_dependencies/src/go_app_with_dependencies' }
 
@@ -66,7 +66,7 @@ describe 'CF Go Buildpack' do
   end
 
   context 'without cached buildpack dependencies' do
-    context 'in an online environment', if: Machete::BuildpackMode.uncached? do
+    context 'in an online environment', :uncached do
       context 'app has dependencies' do
         let(:app_name) { 'go_app_with_dependencies/src/go_app_with_dependencies' }
 
