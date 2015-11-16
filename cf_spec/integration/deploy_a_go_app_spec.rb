91e3221486eff27090484d45b95c5c744ef92715
David Jahn <david.a.jahn@gmail.com>
2015-11-16 16:35:41 -0500

    Add test coverage for issue with Godeps.json
    When an app is using GO15VENDOREXPERIMENT and its Godeps.json does not
    have a "Packages" array the app does not stage. Added test coverage
    (https://github.com/heroku/heroku-buildpack-go/issues/101)
    [#108276804]
    
    Signed-off-by: David Morhovich <dmorhovich@pivotal.io>

--- cf_spec/integration/deploy_a_go_app_spec.rb
+++ cf_spec/integration/deploy_a_go_app_spec.rb
@@ -34,6 +34,20 @@ describe 'CF Go Buildpack' do
       end
     end
 
+    context 'app with vendored dependencies has Godeps.json with no Packages array' do
+      let(:app_name) { 'go15vendorexperiment_no_packages_array/src/go_app' }
+
+      pending 'merge with upstream' do
+        expect(app).to be_running
+        expect(app).to have_logged('Init: a.A == 1')
+
+        browser.visit_path('/')
+        expect(browser).to have_body('Read: a.A == 1')
+
+        expect(app.host).not_to have_internet_traffic
+      end
+    end
+
     context 'app has no dependencies' do
       let(:app_name) { 'go_app/src/go_app' }
 
@@ -121,6 +135,18 @@ describe 'CF Go Buildpack' do
       let(:app_name) { 'go_with_vendor_experiment_flag/src/go_app' }
 
       specify do
+        expect(app).to be_running
+        expect(app).to have_logged('Init: a.A == 1')
+
+        browser.visit_path('/')
+        expect(browser).to have_body('Read: a.A == 1')
+      end
+    end
+
+    context 'app with vendored dependencies has Godeps.json with no Packages array' do
+      let(:app_name) { 'go15vendorexperiment_no_packages_array/src/go_app' }
+
+      pending 'merge with upstream' do
         expect(app).to be_running
         expect(app).to have_logged('Init: a.A == 1')
 
