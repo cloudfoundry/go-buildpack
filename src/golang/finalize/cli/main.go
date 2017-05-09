1aba4c628adaca49e706917001338fc21bfe77e8
Anna Thornton <athornton@pivotal.io>
2017-05-09 17:56:49 -0400

    Make .profile.d scripts and hooks compatible
    - hooks should come before writing to .profile.d directory
    
    [#145118801]
    
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- src/golang/finalize/cli/main.go
+++ src/golang/finalize/cli/main.go
@@ -9,11 +9,11 @@ import (
 )
 
 type config struct {
-  Config struct {
-    GoVersion  string `yaml:"GoVersion"`
-    VendorTool string `yaml:"VendorTool"`
-    Godep      string `yaml:"Godep"`
-  } `yaml:"config"`
+	Config struct {
+		GoVersion  string `yaml:"GoVersion"`
+		VendorTool string `yaml:"VendorTool"`
+		Godep      string `yaml:"Godep"`
+	} `yaml:"config"`
 }
 
 func main() {
@@ -33,13 +33,13 @@ func main() {
 		os.Exit(12)
 	}
 
-	if err := libbuildpack.SetLaunchEnvironment(stager.DepsDir, stager.BuildDir); err != nil {
-		stager.Log.Error("Unable to setup launch environment: %s", err.Error())
+	if err := libbuildpack.RunAfterCompile(stager); err != nil {
+		stager.Log.Error("After Compile: %s", err.Error())
 		os.Exit(13)
 	}
 
-	if err := libbuildpack.RunAfterCompile(stager); err != nil {
-		stager.Log.Error("After Compile: %s", err.Error())
+	if err := libbuildpack.SetLaunchEnvironment(stager.DepsDir, stager.BuildDir); err != nil {
+		stager.Log.Error("Unable to setup launch environment: %s", err.Error())
 		os.Exit(14)
 	}
 
