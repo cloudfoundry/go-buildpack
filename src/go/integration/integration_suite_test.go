ee0acb4b61247b0f20ba7f21c04bbee1628c36c7
Tyler Phelan <tphelan@pivotal.io>
2018-07-24 14:14:10 -0400

    Make tests work on envs without stack association
    - Use cutlass ApiGreaterThan method
    
    [#144940817]
    
    Signed-off-by: Ben Fickes <bfickes@pivotal.io>

--- src/go/integration/integration_suite_test.go
+++ src/go/integration/integration_suite_test.go
@@ -10,7 +10,6 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/blang/semver"
 	"github.com/cloudfoundry/libbuildpack/cutlass"
 
 	. "github.com/onsi/ginkgo"
@@ -34,7 +33,7 @@ func init() {
 var _ = SynchronizedBeforeSuite(func() []byte {
 	// Run once
 	if buildpackVersion == "" {
-		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack(os.Getenv("CF_STACK"))
+		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack(os.Getenv("CF_STACK"), ApiHasStackAssociation())
 		Expect(err).NotTo(HaveOccurred())
 
 		data, err := json.Marshal(packagedBuildpack)
@@ -84,21 +83,22 @@ func Restart(app *cutlass.App) {
 	Eventually(func() ([]string, error) { return app.InstanceStates() }, 20*time.Second).Should(Equal([]string{"RUNNING"}))
 }
 
-func ApiGreaterThan(version string) bool {
-	apiVersionString, err := cutlass.ApiVersion()
-	Expect(err).To(BeNil())
-	apiVersion, err := semver.Make(apiVersionString)
-	Expect(err).To(BeNil())
-	reqVersion, err := semver.ParseRange(">= " + version)
-	Expect(err).To(BeNil())
-	return reqVersion(apiVersion)
+func ApiHasTask() bool {
+	supported, err := cutlass.ApiGreaterThan("2.75.0")
+	Expect(err).NotTo(HaveOccurred())
+	return supported
 }
 
-func ApiHasTask() bool {
-	return ApiGreaterThan("2.75.0")
-}
 func ApiHasMultiBuildpack() bool {
-	return ApiGreaterThan("2.90.0")
+	supported, err := cutlass.ApiGreaterThan("2.90.0")
+	Expect(err).NotTo(HaveOccurred())
+	return supported
+}
+
+func ApiHasStackAssociation() bool {
+	supported, err := cutlass.ApiGreaterThan("2.113.0")
+	Expect(err).NotTo(HaveOccurred())
+	return supported
 }
 
 func AssertUsesProxyDuringStagingIfPresent(fixtureName string) {
