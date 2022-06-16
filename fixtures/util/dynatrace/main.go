aac1e0427557d24927cd37730b33c2117515ced2
Florian Arthofer <florian.arthofer@dynatrace.com>
2022-06-16 20:44:59 +0200

    updated fake dynatrace-api fixture (#264)
    updated Dynatrace integration test with check for config update
    bumped libbuildpack-dynatrace to v1.5.0

--- fixtures/util/dynatrace/main.go
+++ fixtures/util/dynatrace/main.go
@@ -22,26 +22,72 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		switch req.URL.Path {
+		var withoutAgentPath bool
+		path := req.URL.Path
+
+		uri := application.ApplicationURIs[0]
+
+		if strings.HasPrefix(path, "/without-agent-path") {
+			uri = fmt.Sprintf("%s/without-agent-path", uri)
+			path = strings.TrimPrefix(path, "/without-agent-path")
+			withoutAgentPath = true
+		}
+
+		switch path {
 		case "/v1/deployment/installer/agent/unix/paas-sh/latest":
-			context := struct{ URI string }{URI: req.Host}
+			context := struct{ URI string }{URI: uri}
 			t := template.Must(template.New("install.sh").ParseFiles("install.sh"))
 			err := t.Execute(w, context)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprint(w, err.Error())
+				w.Write([]byte(err.Error()))
 				return
 			}
 
-		case "/manifest.json", "/dynatrace-env.sh", "/liboneagentproc.so":
+		case "/dynatrace-env.sh", "/liboneagentproc.so", "/ruxitagentproc.conf":
 			contents, err := ioutil.ReadFile(strings.TrimPrefix(req.URL.Path, "/"))
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprint(w, err.Error())
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.Write(contents)
+
+		case "/manifest.json":
+			var payload map[string]interface{}
+			file, err := os.Open("manifest.json")
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
 				return
 			}
 
-			fmt.Fprintf(w, "%s", contents)
+			err = json.NewDecoder(file).Decode(&payload)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			if withoutAgentPath {
+				payload["technologies"] = map[string]interface{}{
+					"process": map[string]interface{}{
+						"linux-x86-64": []struct{}{},
+					},
+				}
+			}
+
+			json.NewEncoder(w).Encode(payload)
+
+		case "/v1/deployment/installer/agent/processmoduleconfig":
+			fakeConfig, err := ioutil.ReadFile("fake_config.json")
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.Write(fakeConfig)
+
 
 		default:
 			w.WriteHeader(http.StatusNotFound)
